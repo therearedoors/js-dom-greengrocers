f2c0c3f408391885fafcfd518f7f213a8c44e44a
Paul Smart <pdsmart6@gmail.com>
2022-02-12 23:15:22 +0000

    progress on adding to cart

--- index.js
+++ index.js
@@ -53,3 +53,90 @@ const state = {
   ],
   cart: []
 };
+
+const store = document.querySelector(".store--item-list")
+const cart = document.querySelector(".cart--item-list")
+
+function renderStore(){
+  for(let i=0;i<state.items.length;i++){
+    store.append(getStoreListItem(state.items[i]))
+  }
+}
+
+function getStoreListItem(item){
+  const li = document.createElement("li")
+  const div = getDiv()
+  div.append(getImage(item))
+  li.append(div)
+  li.append(getStoreButton(item))
+  return li
+}
+
+function getDiv(){
+  const div = document.createElement("div")
+  div.className = "store--item-icon"
+  return div
+}
+
+function addToCart(item){
+  
+  state.cart.push(item)
+  renderCart()
+}
+
+function getStoreButton(item){
+  const button = document.createElement("button")
+  button.innerText = "Add to cart"
+  button.addEventListener("click", function() {addToCart(item)})
+  return button
+}
+
+function getImage(item){
+  const img = document.createElement('img')
+  img.setAttribute('src', `assets/icons/${item.id}.svg`)
+  img.setAttribute('alt', item.name)
+  return img
+}
+
+function renderCart() {
+  cart.innerHTML = ""
+  for(let i=0;i<state.cart.length;i++){
+    const li = getCartListItem(state.cart[i])
+    cart.appendChild(li)
+  }
+}
+
+function getCartListItem(item){
+  const li = document.createElement("li")
+  li.append(getImage(item), getParagraph(item.name), getRemoveButton(), getSpan(),getAddButton())
+  return li
+}
+
+function getParagraph(text){
+  const p = document.createElement('p')
+  p.innerText = text
+  return p
+}
+
+function getRemoveButton(){
+  const button = document.createElement('button')
+  button.classList.add("quantity-btn","remove-btn","center")
+  button.innerText = "-"
+  return button
+}
+
+function getAddButton(){
+  const button = document.createElement('button')
+  button.classList.add("quantity-btn","add-btn","center")
+  button.innerText = "+"
+  return button
+}
+
+function getSpan(){
+  const span = document.createElement('span')
+  span.classList.add("quantity-text","center")
+  span.innerText = 1
+  return span
+}
+
+renderStore()
